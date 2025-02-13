644f698c1110c9dbb2c0643ec5f85015e296e7a9
Sadrul <sadrul.email@gmail.com>
2025-02-13 09:54:08 +0600

    modified

--- Scripts/deploy_demodb.sql
+++ Scripts/deploy_demodb.sql
@@ -1,7 +1,3 @@
 EXECUTE IMMEDIATE FROM './Databases/DEMODB/demodb_def.sql';
 EXECUTE IMMEDIATE FROM './Databases/DEMODB/Schemas/SCH_DEMO/sch_demo.sql';
-
 EXECUTE IMMEDIATE FROM './Databases/DEMODB/Schemas/SCH_DEMO/Tables/dept_def.sql';
-
-
-
